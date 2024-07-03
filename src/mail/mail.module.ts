0c9f3ee693d7ff9bc9b257e5eabe31b5b62b0f7f
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-03 10:03:51 +0530

    Create mail.module.ts

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,22 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('EMAIL_HOST'),
+          auth: {
+            user: configService.get<string>('EMAIL_USERNAME'),
+            pass: configService.get<string>('EMAIL_PASSWORD'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MailModule {}
