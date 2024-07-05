b26d46db22a5cdf302b3feec9deac3f10f0043bf
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-05 09:21:18 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('3DCart')
+    .setDescription("The ecommerce API's")
+    .setVersion('1.0')
+    .addServer('http://localhost:3000')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'jwt',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
