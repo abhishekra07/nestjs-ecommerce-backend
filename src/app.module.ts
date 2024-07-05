0d0ed017ff6820003a9c5508712a1a2d78648b75
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-05 09:39:53 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { MailModule } from './mail/mail.module';
+import { ProductModule } from './product/product.module';
+import { AccountModule } from './account/account.module';
+import { ShoppingCartController } from './cart/shopping-cart.controller';
+import { ShoppingCartModule } from './cart/shopping-cart.module';
+import { CategoriesModule } from './category/categories.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { ReviewAndRatingModule } from './review/review-rating.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
@@ -12,6 +21,14 @@ import { MailModule } from './mail/mail.module';
     DatabaseModule,
     MailModule,
     UserModule,
+    ProductModule,
+    AccountModule,
+    ShoppingCartModule,
+    CategoriesModule,
+    OrderModule,
+    PaymentModule,
+    ReviewAndRatingModule,
+    SearchModule,
   ],
 })
 export class AppModule {}
