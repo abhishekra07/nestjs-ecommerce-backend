e2fc8dd5f97cbcc039b4af0b16c4545212e2fb35
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-06 14:37:58 +0530

    Update categories.service.ts

--- src/category/categories.service.ts
+++ src/category/categories.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryRepository } from './repository/category.repository';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  createCategory(categoryRequest: CreateCategoryDto) {
+    const existingCategory = this.categoryRepository.find({
+      where: { name: categoryRequest.name },
+    });
+    if (existingCategory) {
+      throw new Error('Category already exists');
+    }
+    const category: Category = this.categoryRepository.create(categoryRequest);
+    return this.categoryRepository.save(category);
+  }
+
+  getAllCategories() {
+    return this.categoryRepository.find();
+  }
+}
