0fe8727e654a65f9f8a6a8764bb1232aab8f08e0
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-06 14:38:03 +0530

    Update categories.module.ts

--- src/category/categories.module.ts
+++ src/category/categories.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Category } from './entities/category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryRepository } from './repository/category.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, CategoryRepository],
 })
 export class CategoriesModule {}
