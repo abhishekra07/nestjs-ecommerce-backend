95e57c6800fd87cfe745d0121a7a43b27fb3b524
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-08 18:54:57 +0530

    Update product.module.ts

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductRepository } from './repository/product.repository';
+import { Product } from './entities/product.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
   providers: [ProductService, ProductRepository],
 })
