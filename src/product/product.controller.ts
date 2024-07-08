7f977ebd98a80cff412366637ba97821340162d7
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-08 18:54:47 +0530

    Update product.controller.ts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,30 +6,33 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @ApiTags('products')
 @Controller('products')
+@UseGuards(JwtAuthGuard)
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
   async getAllProducts() {
-    // return this.productService.getAllProducts();
+    return await this.productService.getAllProducts();
   }
 
   @Get(':id')
   async getProductById(@Param('id') id: number) {
-    // return this.productService.getProductById(id);
+    // return await this.productService.getProductById(id);
   }
 
   @Post()
   async createProduct(@Body() createProductDto: CreateProductDto) {
-    // return this.productService.createProduct(createProductDto);
+    return await this.productService.createProduct(createProductDto);
   }
 
   @Patch(':id')
