3b7c4db16d2f7467b87dd204620b0a6725b98268
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-08 18:55:08 +0530

    Update product.service.ts

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRepository } from './repository/product.repository';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async getAllProducts() {
+    return await this.productRepository.find();
+  }
+
+  async createProduct(productRequest: CreateProductDto) {
+    const product: Product = this.productRepository.create(productRequest);
+    return await this.productRepository.save(product);
+  }
+}
