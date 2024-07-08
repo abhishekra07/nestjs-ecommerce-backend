7f41b5260b4136805b01980b527a84407573415a
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-08 18:55:36 +0530

    Update product.repository.ts

--- src/product/repository/product.repository.ts
+++ src/product/repository/product.repository.ts
@@ -1,6 +1,10 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Product } from '../entities/product.entity';
 
 @Injectable()
-export class ProductRepository extends Repository<Product> {}
+export class ProductRepository extends Repository<Product> {
+  constructor(private dataSource: DataSource) {
+    super(Product, dataSource.createEntityManager());
+  }
+}
