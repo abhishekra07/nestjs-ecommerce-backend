2829fafe60b29370ceb8fc6bd0c9bf268b2317c9
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-09 08:44:12 +0530

    Update user.repository.ts

--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -1,6 +1,10 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+}
